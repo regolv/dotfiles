328921fa0894d5c02457fe68bbee63a49a069e65
Roberts Gulans <roberts@rego.lv>
2014-07-06 15:27:56 +0300

    Apache porting to vagrant

---
+++ dev/install.zsh
@@ -0,0 +1,14 @@
+#! /usr/bin/env zsh
+
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Allows to get to vagrant sites without 8000 port
+cat > /etc/apache2/other/homestead.conf <<EOL
+<VirtualHost *:80>
+    ProxyPreserveHost On
+    ProxyRequests Off
+    ProxyPass / http://localhost:8000/
+    ProxyPassReverse / http://localhost:8000/
+</VirtualHost>
+EOL
