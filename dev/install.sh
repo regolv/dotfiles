8d29d8c088f738ed594de997530cafb5e52469d6
Roberts Guļāns <roberts@rego.lv>
2014-11-20 20:24:07 +0200

    Sudo for default apache port redirect

--- dev/install.sh
+++ dev/install.sh
@@ -4,7 +4,7 @@
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Allows to get to vagrant sites without 8000 port
-cat > /etc/apache2/other/homestead.conf <<EOL
+sudo cat > /etc/apache2/other/homestead.conf <<EOL
 <VirtualHost *:80>
     ProxyPreserveHost On
     ProxyRequests Off
